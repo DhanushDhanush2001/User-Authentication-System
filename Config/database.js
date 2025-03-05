a27e82952670744ebd436d009673a893f864fb7b
DhanushDhanush2001 <dhanushpandian.0207@gmail.com>
2025-03-05 08:31:50 +0530

    updating DB

--- Config/database.js
+++ Config/database.js
@@ -2,7 +2,13 @@ const mongoose = require('mongoose')
 
 const connectDatabase = async () =>{
     try{
-     await mongoose.connect(process.env.DB_LOCAL_URI)
+     await mongoose.connect(process.env.MONGO_URI,{
+         useNewUrlParser: true,
+            useUnifiedTopology: true,
+            tls: true,  // Ensure TLS is enabled
+            serverSelectionTimeoutMS: 5000,  // Timeout after 5s if no response
+            socketTimeoutMS: 45000,
+   });
         console.log(`mongoDb is connect to the host : ${mongoose.connection.host}`)
     } catch(error){
           console.log("DataBase Connection error", error);
